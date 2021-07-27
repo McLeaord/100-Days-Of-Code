6f00a03282dd9fe45700e034e9bea088be8c5911
McLeaord <86162687+McLeaord@users.noreply.github.com>
2021-07-27 20:28:02 +0530

    Add files via upload

---
+++ Dice.py
@@ -0,0 +1,12 @@
+import random
+minimum = 1
+maximum = 6
+roll_more = "y"
+
+
+while roll_more == "y":
+    random.randrange
+    print("Rolling Dice")
+    print("Player(A) : ", random.randint(minimum,maximum))
+    print("Player(B) :", random.randint(minimum,maximum))
+    roll_more = input("Roll Again?")
