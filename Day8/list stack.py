fed4920961bea0c6e9f4e0913240a391e22a36ac
McLeaord <86162687+McLeaord@users.noreply.github.com>
2021-07-31 20:58:49 +0530

    Add files via upload

---
+++ Day8/list stack.py
@@ -0,0 +1,21 @@
+# Implementing Stack Using List
+x = []
+
+#.append here is used to add the names or elements to the List
+x.append("Apple")
+x.append("Banana")
+x.append("cat")
+x.append("Dog")
+
+print("Starting stack")
+print(x)
+
+# .Pop here is used to remove the Lastly added  names or elements from the List
+print("Names poped from stack")
+print(x.pop())
+print(x.pop())
+
+# Using print(x) we can print the remaing names left in the List 
+print("stack after names are poped")
+print(x)
+
