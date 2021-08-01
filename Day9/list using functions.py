d40651016598b0613555ee5a5beeaa6c6cee6ef2
McLeaord <86162687+McLeaord@users.noreply.github.com>
2021-08-01 19:42:56 +0530

    Add files via upload

---
+++ Day9/list using functions.py
@@ -0,0 +1,15 @@
+list1 = [2, 2]
+list2 = [4, 4]
+list =list1 + list2
+
+
+def x(list1, list2):
+    return list1 + list2
+
+
+	
+total = x(list1, list2)
+
+
+
+print("Sum of all elements in given lists: ", total)
