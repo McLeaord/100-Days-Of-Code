e0b624935ba7ad48b85b0ad97536d8632f5ac3eb
McLeaord <86162687+McLeaord@users.noreply.github.com>
2021-08-10 22:47:23 +0530

    Add files via upload

---
+++ Day18/dict.py
@@ -0,0 +1,13 @@
+colors = {1:'Red', 3:'Green', 5:'Black' , 2:'White', 4:'Pink'}
+
+def sort(d, reverse = True):
+  return dict(sorted(d.items(), key = lambda x: x[1], reverse = reverse))
+
+print("Given Elements : ")
+print(colors)
+print()
+print("Ascending Order : ")
+print(sort(colors,False))
+print()
+print("Descending Order : ")
+print(sort(colors, True))
