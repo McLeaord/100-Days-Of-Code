15f1fd4f03c25493269a89eabc6792020d260039
McLeaord <86162687+McLeaord@users.noreply.github.com>
2021-08-10 23:21:30 +0530

    Add files via upload

---
+++ Day18/comprehension.py
@@ -0,0 +1,8 @@
+colors = {'Red', 'Green', 'Black', 'White', 'Pink'}
+fruits = {"Apple", "Mango", "guva", "Litchi", "Grapes"}
+print(colors)
+print(fruits)
+
+y = [x for x in fruits if x!="guva"]
+z = [x for x in colors if "e" in x]
+print(y,z)
