753a8f153b5cc507d56b2b4de7dd273f05477a79
McLeaord <86162687+McLeaord@users.noreply.github.com>
2021-08-14 23:02:07 +0530

    Delete email.py

--- Day21/email.py
+++
@@ -1,7 +0,0 @@
-import smtplib
-
-server = smtplib.SMTP_SSL("smtp.gmail.com",465)
-server.login("example@gmail.com","password")
-server.sendmail("example@gmail.com","contact@gmail.com","HELLO")
-
-server.quit()
